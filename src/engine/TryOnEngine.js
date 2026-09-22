7456898e01af42c1cb683190ed7c428791f504bf
dgrangestao-ops <dgran.gestao@gmail.com>
2026-09-22 18:57:15 -0300

    Corrige carregamento do MediaPipe WASM

--- src/engine/TryOnEngine.js
+++ src/engine/TryOnEngine.js
@@ -7,7 +7,7 @@ export class TryOnEngine {
   }
   async init(){
     this.onStatus("Carregando rastreamento facial…");
-    const vision=await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision@0.10.22/wasm");
+    const vision=await FilesetResolver.forVisionTasks("https://cdn.jsdelivr.net/npm/@mediapipe/tasks-vision/wasm");
     this.landmarker=await FaceLandmarker.createFromOptions(vision,{
       baseOptions:{modelAssetPath:"https://storage.googleapis.com/mediapipe-models/face_landmarker/face_landmarker/float16/1/face_landmarker.task",delegate:"GPU"},
       runningMode:"VIDEO",numFaces:1,outputFacialTransformationMatrixes:true
