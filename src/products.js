f16124cb0bdb489b17b3d95cd83819a5e02f63f5
dgrangestao-ops <dgran.gestao@gmail.com>
2026-09-24 22:11:22 -0300

    Cria catalogo base de armacoes para evolucao multi SKU

---
+++ src/products.js
@@ -0,0 +1,13 @@
+export const PRODUCTS = [
+  {
+    id: "fremi-piloto",
+    brand: "Fremi",
+    name: "Fremi Piloto",
+    sku: "FREMI-PILOTO",
+    modelUrl: null,
+    calibration: { scale: 1, position: [0,0,0], rotation: [0,0,0] },
+    available: true
+  }
+];
+
+export const DEFAULT_PRODUCT_ID = "fremi-piloto";
