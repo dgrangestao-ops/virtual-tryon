64ecf188c4227020a50364c371ed1984b43775a9
dgrangestao-ops <dgran.gestao@gmail.com>
2026-09-25 14:35:45 -0300

    Cadastra primeiro produto real Fremi no catalogo do try on

--- src/products.js
+++ src/products.js
@@ -1,5 +1,17 @@
 export const PRODUCTS = [
   {
+    id: "oculos-esportivo-preto-lente-preta-98un1",
+    brand: "Fremi",
+    name: "Óculos esportivo preto lente preta",
+    sku: "98un1",
+    productUrl: "https://www.fremieyewear.com.br/produtos/oculos-esportivo-preto-lente-preta-98un1/",
+    sourceImageUrl: "https://dcdn-us.mitiendanube.com/stores/005/876/852/products/20251112_135701_-0f9bafd9c75984685117630012588680-1024-1024.webp",
+    modelUrl: null,
+    imageAssetUrl: null,
+    calibration: { scale: 1, position: [0,0,0], rotation: [0,0,0] },
+    available: true
+  },
+  {
     id: "fremi-piloto",
     brand: "Fremi",
     name: "Fremi Piloto",
